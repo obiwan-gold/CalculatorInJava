1f72ea882c28549441de8c2ac7b30c89c2eff132
obiwan-gold <individualog@gmail.com>
2023-04-27 13:36:09 +0100

    Created methods for Calculator class and Input Handler class

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,30 @@
 public class Calculator {
+
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        InputHandler inputHandler = new InputHandler();
+
+        int a = inputHandler.readInt("Enter first number: ");
+        int b = inputHandler.readInt("Enter second number: ");
+
+        int selection = inputHandler.readSelection("Type the number to select the method: \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n");
+
+        inputHandler.handleSelection(selection);
+
+    }
 }
--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class InputHandler {
+    Scanner scan = new Scanner(System.in);
+    public int readInt(String prompt){
+        System.out.println(prompt);
+        return scan.nextInt();
+    }
+
+    public int readSelection(String prompt){
+        System.out.println(prompt);
+        int selection = scan.nextInt();
+        return selection;
+    }
+
+    public void handleSelection(int selection) {
+    }
 }
